7f85df75e3f0637badf3685e45e620e7d14f1e83
agliucci <aliucci@umass.edu>
2025-05-16 17:40:20 -0400

    Add observer that checks for change in submission status

--- dist/content.js
+++ dist/content.js
@@ -1,2 +1,23 @@
 "use strict";
 console.log("LeetCommit loaded on LeetCode");
+const target = document.querySelector('[data-e2e-locator="submission-result"]');
+if (target) {
+    const observer = new MutationObserver(entries => {
+        for (const entry of entries) {
+            if (entry.type === "characterData" || entry.type === "childList") {
+                const newText = target.textContent?.trim();
+                if (newText === "Accepted") {
+                    console.log("Submission accepted");
+                    const submission = {
+                        title: document.querySelector('a[href^="/problems"]'),
+                    };
+                }
+            }
+        }
+    });
+    observer.observe(target, {
+        childList: true,
+        subtree: true,
+        characterData: true,
+    });
+}
--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,26 @@
 console.log("LeetCommit loaded on LeetCode");
+
+const target: Element | null = document.querySelector('[data-e2e-locator="submission-result"]');
+
+if (target) {
+    const observer = new MutationObserver(entries => {
+        for (const entry of entries) {
+            if (entry.type === "characterData" || entry.type === "childList") {
+                const newText = target.textContent?.trim();
+                if (newText === "Accepted") {
+                    console.log("Submission accepted");
+                    const submission = {
+                        title: document.querySelector('a[href^="/problems"]'),
+                       
+                    }
+                }
+            }
+        }
+    });
+
+    observer.observe(target, {
+        childList: true,
+        subtree: true,
+        characterData: true,
+    });
+}
